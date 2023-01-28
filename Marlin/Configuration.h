73b7564a238dbded6fc67ba9df37f606d3ef1676
rOxANDtOl <rodrigosgdlcc@gmail.com>
2023-01-28 17:48:00 +0100

    move offset 2mm (try to fix problem)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1545,7 +1545,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -51, -12, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -51, -14, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
